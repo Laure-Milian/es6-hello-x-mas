1d676110c2dc2b5c35fbf5d2158e7e170035de56
Laure <laure.milian@gmail.com>
2016-12-19 13:48:03 +0100

    version fonctionnelle avec if

--- public/app.js
+++ public/app.js
@@ -5,20 +5,20 @@
 	var button = document.getElementById('button');
 
 	button.addEventListener('click', function () {
-		var content = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'test';
-
 		var input = document.getElementById('input');
 		var output = document.getElementById('output');
-		content = input.value;
-		console.log(content);
-		output.innerHTML = 'Bonjour ' + content + ' !';
+		var content = input.value;
+		if (input.value !== "") {
+			name = input.value;
+			hello(name);
+		} else {
+			hello();
+		}
 	});
+
+	var hello = function hello() {
+		var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Père Noël';
+
+		output.innerHTML = 'Bonjour ' + name + ' !';
+	};
 })();
-
-var hello = function hello() {
-	var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "TomTom";
-
-	console.log('hello' + name);
-};
-
-hello();
--- src/app.es6.js
+++ src/app.es6.js
@@ -2,18 +2,20 @@
 
 	let button = document.getElementById('button');
 
-	button.addEventListener('click', (content= 'test') => {
+	button.addEventListener('click', () => {
 		let input = document.getElementById('input');
 		let output = document.getElementById('output');
-		content = input.value;
-		console.log(content);
-		output.innerHTML = `Bonjour ${content} !`;
+		let content = input.value;
+		if (input.value !== "") {
+			name = input.value;
+			hello(name);
+		} else {
+			hello();
+		}
 	});
 
-})();
-
-	const hello = (name= "TomTom") => {
-		console.log('hello' + name);
+	const hello = (name= 'Père Noël') => {
+		output.innerHTML = `Bonjour ${name} !`;
 	};
 
-	hello();
+})();

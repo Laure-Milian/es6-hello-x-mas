82a0084b1bdba028191170f4304205a4b5079d51
Laure <laure.milian@gmail.com>
2016-12-19 16:22:55 +0100

    step 3 fonctionnelle

--- public/app.js
+++ public/app.js
@@ -2,16 +2,16 @@
 
 (function () {
 
-	//Step 1
 	var button = document.getElementById('button');
 	var input = document.getElementById('input');
 	var output = document.getElementById('output');
 
-	// Step 2
 	var listOfNames = document.getElementById('listOfNames');
 	var namesArray = [];
 
-	// Step 1
+	var inputSearch = document.getElementById('inputSearch');
+	var buttonSearch = document.getElementById('buttonSearch');
+
 	button.addEventListener('click', function (event) {
 		var name = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "Père Noël";
 
@@ -22,7 +22,6 @@
 		output.innerHTML = 'Bonjour ' + name;
 	});
 
-	//Step2
 	var capitalizeName = function capitalizeName(name) {
 		var goodName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 		addToArray(goodName);
@@ -30,31 +29,34 @@
 
 	var addToArray = function addToArray(goodName) {
 		namesArray.push(goodName);
-		display();
+		display(namesArray);
 	};
-
-	var display = function display() {
-		listOfNames.innerHTML = '';
-		var len = namesArray.length;
-		for (var i = 0; i < len; i++) {
-			var newLine = document.createElement('li');
-			newLine.innerHTML = namesArray[i];
-			listOfNames.appendChild(newLine);
-		}
-	};
-
-	//Step 3
-	var inputSearch = document.getElementById('inputSearch');
-	var buttonSearch = document.getElementById('buttonSearch');
 
 	buttonSearch.addEventListener('click', function () {
 		if (inputSearch.value !== "") {
-			var search = inputSearch.value;
-			console.log(search);
-			var len = namesArray.length;
-			for (var i = 0; i < len; i++) {
-				console.log(namesArray[i]);
-			}
+			(function () {
+				var search = inputSearch.value;
+				search = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase();
+				var lenSearch = search.length;
+
+				var newArray = namesArray.filter(function (e) {
+					var sliceValue = e.slice(0, lenSearch);
+					if (sliceValue === search) {
+						return e;
+					}
+				});
+				display(newArray);
+			})();
 		}
 	});
+
+	var display = function display(arrayName) {
+		listOfNames.innerHTML = '';
+		var len = arrayName.length;
+		for (var i = 0; i < len; i++) {
+			var newLine = document.createElement('li');
+			newLine.innerHTML = arrayName[i];
+			listOfNames.appendChild(newLine);
+		}
+	};
 })();
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,15 +1,15 @@
 (() => {
 
-	//Step 1
 	const button = document.getElementById('button');
 	const input = document.getElementById('input');
 	const output = document.getElementById('output');
 
-	// Step 2
 	let listOfNames = document.getElementById('listOfNames');
 	let namesArray = [];
 	
-	// Step 1
+	const inputSearch = document.getElementById('inputSearch');
+	const buttonSearch = document.getElementById('buttonSearch');
+
 	button.addEventListener('click', (event, name="Père Noël") => {
 		if (input.value !== "") {
 			name = input.value;
@@ -18,7 +18,6 @@
 		output.innerHTML = `Bonjour ${name}`;
 	});
 
-	//Step2
 	const capitalizeName = (name) => {
 		let goodName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 		addToArray(goodName);
@@ -26,33 +25,33 @@
 
 	const addToArray = (goodName) => {
 		namesArray.push(goodName);
-		display();
+		display(namesArray);
 	};
-
-	const display = () => {
-		listOfNames.innerHTML = '';
-		let len = namesArray.length;
-		for (let i = 0; i < len; i++) {
-			let newLine = document.createElement('li');
-			newLine.innerHTML = namesArray[i];
-			listOfNames.appendChild(newLine);
-		}
-	};
-
-	//Step 3
-	const inputSearch = document.getElementById('inputSearch');
-	const buttonSearch = document.getElementById('buttonSearch');
 
 	buttonSearch.addEventListener('click', () => {
 		if (inputSearch.value !== "") {
 			let search = inputSearch.value;
-			console.log(search);
-			let len = namesArray.length;
-			for (let i = 0; i < len; i++) {
-				console.log(namesArray[i]);
-			}
+			search = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase();
+			let lenSearch = search.length;
+
+			let newArray = namesArray.filter((e) => {
+				let sliceValue = e.slice(0, lenSearch);
+				if (sliceValue === search) {
+					return e;
+				}
+			})
+			display(newArray);
 		}
 	});
 
+	const display = (arrayName) => {
+		listOfNames.innerHTML = '';
+		let len = arrayName.length;
+		for (let i = 0; i < len; i++) {
+			let newLine = document.createElement('li');
+			newLine.innerHTML = arrayName[i];
+			listOfNames.appendChild(newLine);
+		}
+	};
 
 })();

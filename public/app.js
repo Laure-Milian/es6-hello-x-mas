0ec83755aa0cbfe9f3c815eb24fd8437e78dc7b2
Laure <laure.milian@gmail.com>
2016-12-19 15:12:33 +0100

    step 2 fonctionnel

--- public/app.js
+++ public/app.js
@@ -2,16 +2,40 @@
 
 (function () {
 
+	//Step 1
 	var button = document.getElementById('button');
 	var input = document.getElementById('input');
 	var output = document.getElementById('output');
 
+	// Step 2
+	var listOfNames = document.getElementById('listOfNames');
+	var namesArray = [];
+
+	// Step 1
 	button.addEventListener('click', function (event) {
 		var name = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "Père Noël";
 
 		if (input.value !== "") {
 			name = input.value;
+			capitalizeName(name);
 		}
 		output.innerHTML = 'Bonjour ' + name;
 	});
+
+	//Step2
+	var capitalizeName = function capitalizeName(name) {
+		var goodName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+		addToArray(goodName);
+		display(goodName);
+	};
+
+	var addToArray = function addToArray(goodName) {
+		namesArray.push(goodName);
+	};
+
+	var display = function display(goodName) {
+		var newLine = document.createElement('li');
+		newLine.innerHTML = goodName;
+		listOfNames.appendChild(newLine);
+	};
 })();
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,14 +1,38 @@
 (() => {
 
+	//Step 1
 	const button = document.getElementById('button');
 	const input = document.getElementById('input');
 	const output = document.getElementById('output');
 
+	// Step 2
+	let listOfNames = document.getElementById('listOfNames');
+	let namesArray = [];
+	
+	// Step 1
 	button.addEventListener('click', (event, name="Père Noël") => {
 		if (input.value !== "") {
 			name = input.value;
+			capitalizeName(name);
 		}
 		output.innerHTML = `Bonjour ${name}`;
 	});
+
+	//Step2
+	const capitalizeName = (name) => {
+		let goodName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+		addToArray(goodName);
+		display(goodName);
+	};
+
+	const addToArray = (goodName) => {
+		namesArray.push(goodName);
+	};
+
+	const display = (goodName) => {
+		let newLine = document.createElement('li');
+		newLine.innerHTML = goodName;
+		listOfNames.appendChild(newLine);
+	};
 
 })();

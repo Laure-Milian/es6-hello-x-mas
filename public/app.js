6a5e057953375a8bb059433c6c2737472d1a19b4
Laure <laure.milian@gmail.com>
2016-12-19 13:56:29 +0100

    clean for code review

--- public/app.js
+++ public/app.js
@@ -3,10 +3,10 @@
 (function () {
 
 	var button = document.getElementById('button');
+	var input = document.getElementById('input');
+	var output = document.getElementById('output');
 
 	button.addEventListener('click', function () {
-		var input = document.getElementById('input');
-		var output = document.getElementById('output');
 		var content = input.value;
 		if (input.value !== "") {
 			name = input.value;
@@ -19,6 +19,6 @@
 	var hello = function hello() {
 		var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Père Noël';
 
-		output.innerHTML = 'Bonjour ' + name + ' !';
+		output.innerHTML = 'Bonjour ' + name;
 	};
 })();
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,21 +1,22 @@
 (() => {
 
-	let button = document.getElementById('button');
+	const button = document.getElementById('button');
+	const input = document.getElementById('input');
+	const output = document.getElementById('output');
 
-	button.addEventListener('click', () => {
-		let input = document.getElementById('input');
-		let output = document.getElementById('output');
+	button.addEventListener('click', () => {		
 		let content = input.value;
 		if (input.value !== "") {
 			name = input.value;
 			hello(name);
-		} else {
+		}
+		else {
 			hello();
 		}
 	});
 
 	const hello = (name= 'Père Noël') => {
-		output.innerHTML = `Bonjour ${name} !`;
+		output.innerHTML = `Bonjour ${name}`;
 	};
 
 })();
